e3daebd8de9fb4bcf14db495347419483a840633
rajith27 <rajithvgopalm@gmail.com>
2024-09-18 12:06:08 -0400

    dynamic routing

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
+// Import the express library to create an Express app
 const express = require('express');
+
+// Initialize the Express application
 const app = express();
 
-// This will only match GET call to /user
-app.get("/user", (req, res) => {
-    res.send({"firstName" : "Rajith", "lastname" : "Gopal"});
+// This will only respond to GET requests made to "/user"
+app.get("/user/:userId?", (req, res) => {
+    console.log(req.params);
+    res.send({firstName : "Rajith", lastname : "Gopal"});
+});
 
-})
-
-
+// Start the server and listen on port 3000 for incoming connections
 app.listen(3000, () => {
     console.log("server started successfully on port 3000...");
 });
