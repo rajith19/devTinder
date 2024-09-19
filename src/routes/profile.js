f8590eb68dd478432d365dfe807e2932666ff46a
rajith27 <rajithvgopalm@gmail.com>
2024-09-19 18:45:16 -0400

    created edit profile

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,17 +1,37 @@
 const express = require("express");
 const profileRouter = express.Router();
 const { userAuth } = require("../middlewares/auth")
+const { validateEditProfileData } = require("../utils/validation")
 
 
-profileRouter.get("/profile", userAuth, async (req, res) => {
+profileRouter.get("/view", userAuth, async (req, res) => {
     try {
         res.send([req.user]);
     }
     catch (err) {
         res.status(400).send("ERROR : " + err.message);
     }
-
 })
 
+profileRouter.patch("/edit", userAuth, async (req, res) => {
+    try {
+        console.log("started")
+        if (!validateEditProfileData(req)) {
+            throw new Error("Invalid Requests!");
+        }
+        const loggedInUser = req.user;
+        Object.keys(req.body).forEach(key => loggedInUser[key] = req.body[key]);
+
+        loggedInUser.save();
+        res.send({
+            message: `${loggedInUser.firstName}, your profile was updated successfully`,
+            data: loggedInUser
+        })
+
+    }
+    catch (err) {
+        res.status(400).send("ERROR : " + err.message);
+    }
+})
 
 module.exports = profileRouter;
--- src/utils/validation.js
+++ src/utils/validation.js
@@ -13,6 +13,46 @@ const validateSignUpData = (req) => {
 
 }
 
+const validateEditProfileData = (req) => {
+
+    const allowedEditFields = ["firstName", "lastName", "age", "about", "photoUrl", "skills", "gender"];
+
+    const { photoUrl, age, about, gender, skills } = req.body;
+
+
+    // Validate photoUrl
+    if (!validator.isURL(photoUrl)) {
+        throw new Error("Invalid URL added: " + photoUrl);
+    }
+
+    // Validate skills array (must be an array of strings)
+    if (!Array.isArray(skills) || !skills.every(skill => typeof skill === 'string')) {
+        throw new Error("Skills must be an array of strings.");
+    }
+
+    // Validate age (must be a number)
+    if (typeof age !== 'number' || isNaN(age)) {
+        throw new Error("Age must be a valid number.");
+    }
+
+    // Validate about 
+    if (typeof about !== 'string') {
+        throw new Error("About must be a non-empty string");
+    } else if (about.length > 300) {
+        throw new Error("About section is too long (maximum 300 characters)");
+    }
+
+    // Validate gender (must be one of the allowed values)
+    const allowedGenders = ['male', 'female', 'other'];
+    if (!allowedGenders.includes(gender)) {
+        throw new Error(`Invalid gender: ${gender}. Must be one of ${allowedGenders.join(', ')}`);
+    }
+
+    const isAllowedEdit = Object.keys(req.body).every(field => allowedEditFields.includes(field));
+    return isAllowedEdit;
+}
+
 module.exports = {
-    validateSignUpData
+    validateSignUpData,
+    validateEditProfileData
 }
