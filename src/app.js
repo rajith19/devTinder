706db9d97c667c0192268b6ea57c9b3f14a6a00b
rajith27 <rajithvgopalm@gmail.com>
2024-09-19 17:36:17 -0400

    routes refactored

--- src/app.js
+++ src/app.js
@@ -16,9 +16,8 @@ const profileRouter = require("./routes/profile");
 const requestsRouter = require("./routes/requests");
 
 app.use("/", authRouter);
-app.use("/", profileRouter);
-app.use("/", requestsRouter);
-
+app.use("/profile", profileRouter);
+app.use("/requests", requestsRouter);
 
 
 
